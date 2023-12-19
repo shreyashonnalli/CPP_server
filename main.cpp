8eee6259bf9b18fcc14aaeae4e4ff1fd89cb41f7
Shreyas Honnalli <sc20sh@leeds.ac.uk>
2023-12-19 21:19:31 +0000

    f

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     {
         std::cerr << "Usage: " << argv[0] << " <port>"
                   << " <fileLocation>" << std::endl;
+        return -1;
     }
 
     int port = std::stoi(argv[1]);
